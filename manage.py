ea3e7a810cade47ebe5e8cd4bb4c6084a843c8de
Benjamin-23 <kitongabenja34@gmail.com>
2021-02-04 22:10:13 +0300

    edit

--- manage.py
+++ manage.py
@@ -11,4 +11,16 @@ def index():
 def aboutPage():
     return render_template('about_us.html')
 
+@app.route('/contact') 
+def contactUs():
+     return render_template('contact_us.html')  
+
+@app.route('/login') 
+def login():
+    return render_template('login.html') 
+
+@app.route('/register')
+def register():
+    return render_template('register.html')     
+
 app.run()
